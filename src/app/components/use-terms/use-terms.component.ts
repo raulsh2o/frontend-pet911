87c97a902922719397644685ecbbc16b0bb5c29a
raulsh2o <raulsh2o@gmail.com>
2023-12-02 17:27:12 -0500

    se crea sin quemar el tos

--- src/app/components/use-terms/use-terms.component.ts
+++ src/app/components/use-terms/use-terms.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-use-terms',
@@ -9,17 +10,38 @@ import { Router } from '@angular/router';
 })
 export class UseTermsComponent  implements OnInit {
 
-  constructor(private authService:AuthService, private router:Router, ) { }
+  constructor(private authService:AuthService, private router:Router, private storage:Storage) { }
 
   ngOnInit() {}
 
   acept(){
-    var gettos={
+/*     var gettos={
       email:"raulsh2o@gmail.com",
     }
      this.authService.postTos(gettos).subscribe((res:any)=>{
     })
-    this.router.navigate([`client-start`]);
+    this.router.navigate([`client-start`]); */
+
+    this.storage.create().then(() => {
+      console.log('Base de datos creada');
+      this.storage.get('client_email').then((value) => {
+        console.log('Valor recuperado:', value);
+        var gettos={
+          email:value,
+        }
+        this.authService.postTos(gettos).subscribe((res:any)=>{
+        })
+        this.router.navigate([`client-start`]);
+        
+      }).catch(error => {
+        console.error('Error al obtener client_email', error);
+      });
+    }).catch(error => {
+      console.error('Error al crear la base de datos', error);
+    });
+
+
+
   }
 
 }
